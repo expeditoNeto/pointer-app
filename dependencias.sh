f1df08d3ef21023a65abfb5fcf2f5e01169faf80
expeditoNeto <expedito.magalhaes.neto@gmail.com>
2023-12-14 01:47:36 -0300

    Ajuste para rodar automatico

--- dependencias.sh
+++ dependencias.sh
@@ -3,4 +3,4 @@ sudo apt update -y
 sudo apt install git ansible -y
 git clone https://github.com/expeditoNeto/pointer-app.git /tmp/pointer-app/
 cd /tmp/pointer-app
-ansible-playbook pointer-plybook.yml
+ansible-playbook pointer-playbook.yml
